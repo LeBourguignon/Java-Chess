6d48cea4a2604e8243f606757a8c92eb99b7471d
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-06-12 22:56:57 +0200

    up

--- Projet/src/views/Promote.java
+++ Projet/src/views/Promote.java
@@ -11,7 +11,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
